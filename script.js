0ce75fa0954369f2aefe400f849f80257d425021
ayanasalceanu <164663283+ayanasalceanu@users.noreply.github.com>
2024-04-28 18:19:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openTBR(){
+    window.location.href="TBR.html";
+}
+function openreviews(){
+    window.location.href="reviews.html";
+}
+function openfavorite(){
+    window.location.href="favorite.html";
+}
